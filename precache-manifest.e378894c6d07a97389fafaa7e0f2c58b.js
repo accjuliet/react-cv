12e70cb61e4fa248dbd44a28a31e40e28bb8c267
Julieta <juliiy98@gmail.com>
2020-01-15 23:44:18 -0700

    Updates

---
+++ precache-manifest.e378894c6d07a97389fafaa7e0f2c58b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08734da7d0577c01a0869cc6e1519395",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "6f413592f2eb449bd322",
+    "url": "/react-cv/static/css/main.2cff6135.chunk.css"
+  },
+  {
+    "revision": "9e45c83b93216d78122b",
+    "url": "/react-cv/static/js/2.7bd6912f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-cv/static/js/2.7bd6912f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f413592f2eb449bd322",
+    "url": "/react-cv/static/js/main.e28bf834.chunk.js"
+  },
+  {
+    "revision": "bef0d5a6b8d8209f3d15",
+    "url": "/react-cv/static/js/runtime-main.5290697a.js"
+  },
+  {
+    "revision": "e7b9f7922af4bf07baa476bb81e5afb4",
+    "url": "/react-cv/static/media/mypic.e7b9f792.jpeg"
+  }
+]);
